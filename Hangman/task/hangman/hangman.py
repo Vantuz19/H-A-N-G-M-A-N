72a893ce1355b920b5b51054d910b7912a66427e
ivan <imalashenko22@gmail.com>
2022-02-24 22:35:18 +0300

    Add possibility to check presence of guess in secret word and display it

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,10 +1,22 @@
 
 print("H A N G M A N")
 word = __import__("random").choice(('python', 'java', 'kotlin', 'javascript'))
-secret_word = '-' * len(word)
+secret_word = ['-' for _ in range(len(word))]
 list_entered_letter = []
 for _ in range(8):
     print()
-    print(secret_word)
+    print(*secret_word, sep="")
     guess = input(f"Input a letter: ")
+    list_entered_letter.append(guess)
+    if guess in word:
+        for index, letter in enumerate(word):
+            if letter == guess:
+                secret_word[index] = letter
+    else:
+        print("That letter doesn't appear in the word")
+# if "".join(secret_word) == word:
+print("""
+Thanks for playing!
+We'll see how well you did in the next stage""")
+
 
