093295e5c84de373941edb543f60320bfbd95e56
ivan <imalashenko22@gmail.com>
2022-02-24 21:58:21 +0300

    initialisation of game

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,5 +1,5 @@
 
 print("H A N G M A N")
 word = __import__("random").choice(('python', 'java', 'kotlin', 'javascript'))
-guess = input(f"Guess the word {word[:3] + '-' * (len(word) - 3)}:")
+guess = input(f"Guess the word {'-' * len(word)}:")
 print(("You lost!", "You survived!")[guess == word])
