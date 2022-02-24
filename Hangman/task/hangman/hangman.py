9886288f70734a1f0f90d5d76d5f9b803274d9af
ivan <imalashenko22@gmail.com>
2022-02-24 23:21:19 +0300

    added checking if user already guess a letter and try it again

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -3,20 +3,27 @@ print("H A N G M A N")
 word = __import__("random").choice(('python', 'java', 'kotlin', 'javascript'))
 secret_word = ['-' for _ in range(len(word))]
 list_entered_letter = []
-for _ in range(8):
+mistakes = 0
+while mistakes < 8 and "".join(secret_word) != word:
     print()
     print(*secret_word, sep="")
     guess = input(f"Input a letter: ")
-    list_entered_letter.append(guess)
+    if guess in list_entered_letter:
+        print("No improvements")
+        mistakes += 1
+        continue
     if guess in word:
+        list_entered_letter.append(guess)
         for index, letter in enumerate(word):
             if letter == guess:
                 secret_word[index] = letter
     else:
         print("That letter doesn't appear in the word")
-# if "".join(secret_word) == word:
-print("""
-Thanks for playing!
-We'll see how well you did in the next stage""")
+        mistakes += 1
+if "".join(secret_word) != word:
+    print("""You lost!""")
+else:
+    print("""You guessed the word!
+You survived!""")
 
 
